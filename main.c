1321e589dafd2038d76e96e1cd7a578ad2e32d14
sam vesurai <sam94089@gmail.com>
2013-02-22 22:24:29 -0800

    improved activity detection
    changed how activity interrupt is generated and detected

--- main.c
+++ main.c
@@ -13,6 +13,7 @@ unsigned char transfer(unsigned char);
 unsigned char Identify();
 void SoftReset();
 void WriteByteToRegister(unsigned char, unsigned char);
+unsigned char ReadStatusRegister();
 
 unsigned char result = 0x0;
 
@@ -20,8 +21,8 @@ int main(void)
 {
 	WDTCTL = WDTPW + WDTHOLD; 		// Stop watchdog timer
 
-	BCSCTL1 = CALBC1_8MHZ;            // Set range
-	DCOCTL = CALDCO_8MHZ;             // Set DCO step + modulation
+	BCSCTL1 = CALBC1_1MHZ;            // Set range
+	DCOCTL = CALDCO_1MHZ;             // Set DCO step + modulation
 
 	P1DIR = BIT6 | BIT5 | BIT1 | BIT0; 		// configure P1.1, P1.5, P1.6 for USI; P1.0 for LED
 
@@ -32,7 +33,7 @@ int main(void)
 	USICKCTL &= ~USICKPL;
 	USICTL1 = USICKPH;
 
-	USICKCTL |= USIDIV_2 | USISSEL_2;		// SMCLK = 2 MHz
+	USICKCTL |= USIDIV_2 | USISSEL_2;		// SPI clock
 
 	USICTL0 &= ~USISWRST; 		// release USI for operation
 
@@ -41,53 +42,32 @@ int main(void)
 
 	SoftReset();
 
-	result = Identify();
-
 	// Turn on LED if ADXL362 is not found
+	result = Identify();
 	if (result==0xAD)
 		P1OUT &= ~BIT0;
 	else
 		P1OUT |= BIT0;
 
-	SoftReset();
-
-	// set activity threshold
-	//WriteByteToRegister( 0x20, 0xFA );		// 250mg
-	WriteByteToRegister( 0x20, 0x64 );			// 100mg
+	// set activity threshold to 100 mg
+	WriteByteToRegister( 0x20, 0x64 );
 	WriteByteToRegister( 0x21, 0x0 );
 
-	// set inactivity threshold
-	//WriteByteToRegister( 0x23, 0x96 );			// 150mg
-	WriteByteToRegister( 0x23, 0x19 );			// 25mg
-	WriteByteToRegister( 0x24, 0x0 );
+	// Default Mode and Referenced Activity only (don't care about Inactivity)
+	WriteByteToRegister( 0x27, 0x3 );
 
-	// set inactivity timer
-	//WriteByteToRegister( 0x25, 0x10 );
-	WriteByteToRegister( 0x25, 0x3 );
-	WriteByteToRegister( 0x26, 0x0 );
+	// map activity status to int1
+	WriteByteToRegister( 0x2A, 0x10 );
 
-	WriteByteToRegister( 0x27, 0x3F );
-
-	// map AWAKE to int1
-	WriteByteToRegister( 0x2A, 0x40 );
-
-	// being measurement
+	// begin measurement in wakeup mode
 	WriteByteToRegister( 0x2D, 0x0A );
 
-	// interupt check on P1.3
+	// interrupt check on P1.3
 	P1IE = BIT3;
 	P1IES &= ~BIT3;		// rise edge
 	P1IFG &= ~BIT3; 	// P1.3 IFG cleared
 
 	_BIS_SR(LPM4_bits + GIE);
-
-//	for (;;)
-//	{
-//		if ((BIT3 & P1IN))
-//			P1OUT |= BIT0;
-//		else
-//			P1OUT &= ~BIT0;
-//	}
 }
 
 #pragma vector=PORT1_VECTOR
@@ -99,13 +79,18 @@ __interrupt void Port_1(void)
 		volatile unsigned int i;
 
 		P1OUT ^= BIT0;
-		i = 64000;
+		i = 10000;
 		do i--;
 		while(i != 0);
 	}
 
+	// acknowledge interrupt by reading STATUS register
+	ReadStatusRegister();
+
 	P1IFG &= ~BIT3;                           // P1.3 IFG cleared
 
+	// restart measurement in wakeup mode again after interrupt is cleared
+	WriteByteToRegister( 0x2D, 0x0A );
 }
 
 /*
@@ -120,6 +105,19 @@ void WriteByteToRegister(unsigned char address, unsigned char value)
 	transfer(address);	// register address
 	transfer(value);
 	P1OUT |= BIT1;		// disable CS; pull CS high
+}
+
+unsigned char ReadStatusRegister()
+{
+	unsigned char s = 0x0;
+
+	P1OUT &= ~BIT1; 	// CS enable; pull CS low
+	transfer(0xB);		// READ command
+	transfer(0x0B);		// register address for STATUS
+	s = transfer(0xFF);	// dummy send; read data from MISO
+	P1OUT |= BIT1;		// disable CS; pull CS high
+
+	return s;
 }
 
 unsigned char Identify()
